66a4ec437a6e2bccf5ed210085ff59d8fafafe03
phamquanganhcode <phamanhh25@gmail.com>
2026-03-01 02:04:36 +0700

    fix: add branch relationship to Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,9 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    protected $fillable = [
-    'employee_code', 'full_name', 'email', 'phonenumber', 
-    'avatar_url', 'fingerprint_id', 'role', 'branch_id', 
-    'type', 'base_salary', 'status'
-];
+        protected $fillable = [
+        'employee_code', 'full_name', 'email', 'phonenumber', 
+        'avatar_url', 'fingerprint_id', 'role', 'branch_id', 
+        'type', 'base_salary', 'status'
+    ];
+    public function branch()
+        {
+            // 1 Nhân viên thuộc về 1 Cơ sở (Branch)
+            return $this->belongsTo(Branch::class, 'branch_id');
+        }
 }
